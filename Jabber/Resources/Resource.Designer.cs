0a567d8fbc3d9efd75013d9b143e5b59317f0a02
AksumiteGold <abdella.draru@gmail.com>
2017-05-31 03:01:09 +0200

    Deleted layout files that were not in use

--- Jabber/Resources/Resource.Designer.cs
+++ Jabber/Resources/Resource.Designer.cs
@@ -3047,6 +3047,9 @@ namespace Jabber
 			// aapt resource value: 0x7f03003e
 			public const int Welcome_Activity = 2130903102;
 			
+			// aapt resource value: 0x7f03003f
+			public const int Welcome_Activity1 = 2130903103;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
